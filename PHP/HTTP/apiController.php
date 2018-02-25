bf54f3d00e0261522b4b828fe120cdddaea4fbd5
Luis Fernando Valera Bernal <lvalerab@gmail.com>
2018-02-25 17:54:26 +0100

    Añadido la clase apiController que se debe heredar en todos los controladores, incluido el routeController

---
+++ PHP/HTTP/apiController.php
@@ -0,0 +1,18 @@
+<?php
+    class apiController {
+
+        private $_request;
+        private $_response;
+
+        /**
+         * 
+         */
+        public function __construct($request,$response) {
+            $this->_request=$request;
+            $this->_response=$response;
+        }
+
+
+
+    }
+?>
--- PHP/HTTP/response.php
+++ PHP/HTTP/response.php
@@ -3,19 +3,57 @@
 
         private $_HEADERS=[];
         private $_ENVELOP="";
+        private $_ResponseCode=200;
+        private $_ResponseText="OK";
 
-        function __construct() {
+        public function __construct() {
+            //Headers por defecto            
+        }
+
+        /**
+         * Establece que la respuesta no sea cacheada por los navegadores
+         *
+         */
+        public function NoCacheHeader() {
             $this->_HEADERS['Cache-control']="no-cache, must-revalidate";
-            $this->_HEADERS['Expires']="Sat, 26 Jul 1997 00:00:00 GMT";
+            $this->_HEADERS['Expires']="Sat, 26 Jul 1997 00:00:00 GMT";        
         }
 
-        function SetType(mime="text/json",charset="utf8") {
-            $_HEADERS['Content-type']=mime;
-            $_HEADERS['']
+        /**
+         * Establece el tipo mime y la codificación del retorno
+         * @param $mime Tipo mime
+         * @param $charset Codificación del texto a devolver
+         */
+        public function SetType($mime="text/json",$charset="utf-8") {
+            $_HEADERS['Content-type']=$mime.'; charset='.$charset;            
         }
 
-        function __destruct() {
+        /**
+         * Establece el código de respuesta y el texto de la respuesta
+         * @param $code Código de respuesta soportado por el protocolo http
+         * @param $text Texto de la respuesta
+         */
+        public function SetResponseCode($code,$text) {
+            $this->_ResponseCode=$code;
+            $this->_ResponseText=$text;
+        }
 
+        public function __destruct() {
+            unset($this->_HEADERS);
+        }
+
+        
+        /**
+         * Imprime el resultado, almacenado en el envelop
+         */
+        public function GetResponse() {
+            //seteamos los headers;
+            foreach($this->_HEADERS as $head => $valor) {
+                header($head.":".$valor);
+            };
+            header(isset($_SERVER['SERVER_PROTOCOL']?$_SERVER['SERVER_PROTOCOL']:"HTTP/1.0").' '.$this->_ResponseCode.' '.$this->_ResponseText);            
+            echo $this->_ENVELOP;
+            exit;
         }
     }
 ?>
